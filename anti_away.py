cec1436d088eba191f5d2e25e330160adb3626c6
May <baermay98@gmail.com>
2022-10-20 15:12:24 +0200

    Add more options

--- anti_away.py
+++ anti_away.py
@@ -1,11 +1,23 @@
 import pyautogui
 import time
 from random import randint
+import typer
 
-while True:
-    current_position = pyautogui.position()
-    x_direction = randint(-100, 100)
-    y_direction = randint(-100, 100)
+def do_actions(distance: int = 600, left_click: bool = False, press_button: str = None, sleep_interval: int = 60, max_sleep_interval: int = None):
+    while True:
+        if distance > 0:
+            x_direction = randint(-distance, distance)
+            y_direction = randint(-distance, distance)
 
-    pyautogui.moveRel(x_direction, y_direction, 1)
-    time.sleep(5)
+            pyautogui.moveRel(x_direction, y_direction, 1)
+        
+        if left_click:
+            pyautogui.click()
+        
+        if press_button:
+            pyautogui.press(press_button)
+
+        time.sleep(randint(sleep_interval, max_sleep_interval if max_sleep_interval else sleep_interval))
+
+if __name__ == "__main__":
+    typer.run(do_actions)
